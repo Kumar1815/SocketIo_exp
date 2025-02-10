3138af1b3522d9e886a6ee60644437b0c7ba0f33
Prasanna kumar <prasannakumar18151991@gmail.com>
2025-02-10 15:31:04 +0530

    added the hosting auth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 443, // Ensures compatibility with ngrok
+    },
+    allowedHosts: "all", // Allows all hosts (for ngrok)
+  },
+});
