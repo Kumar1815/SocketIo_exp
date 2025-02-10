ce4f66409f09bbbf9bdcc83d792e0a269071e60c
Prasanna kumar <prasannakumar18151991@gmail.com>
2025-02-10 16:43:13 +0530

    latest commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      clientPort: 443, // Ensures compatibility with ngrok
-    },
-    allowedHosts: "all", // Allow all hosts (for ngrok)
-    cors: true, // Enable CORS
+  plugins: [react()],
+  server:{
+    port:3000,
   },
-});
+})
