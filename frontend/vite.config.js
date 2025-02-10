e746f2f7476a57a613161b2e7e6b29ce0b5bfd24
Prasanna kumar <prasannakumar18151991@gmail.com>
2025-02-10 17:50:34 +0530

    changed vite js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
+  server: {
+    port: 3000,
+    allowedHosts: [
+      '55ec-34-74-228-140.ngrok-free.app', // Add the ngrok domain you are using
+      'localhost', // Optional, add localhost if needed
+    ],
   },
 })
